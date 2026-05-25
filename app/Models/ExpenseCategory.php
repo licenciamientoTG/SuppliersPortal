ea02085ddd34e42b578623f09c3c81a9a815d1f3
DanielTG25 <daniel.ramirez@totalgas.com>
2026-05-25 12:34:25 -0600

    z

--- app/Models/ExpenseCategory.php
+++ app/Models/ExpenseCategory.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Auth\Auth;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * ExpenseCategory
